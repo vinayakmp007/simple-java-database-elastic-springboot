3e23241ed159f8d6eeef772a4ee00a1abf9eb222
Vinayak MP <vinayakmp007@gmail.com>
2020-07-14 23:21:20 +0530

    Added function to initialize  tasks when the application is up

--- src/main/java/com/vinayaksproject/simpleelasticproject/SimpleElasticSuggestionBoxApplication.java
+++ src/main/java/com/vinayaksproject/simpleelasticproject/SimpleElasticSuggestionBoxApplication.java
@@ -1,7 +1,12 @@
 package com.vinayaksproject.simpleelasticproject;
 
+import com.vinayaksproject.simpleelasticproject.enums.IndexJobType;
+import com.vinayaksproject.simpleelasticproject.services.TaskManagementService;
+import com.vinayaksproject.simpleelasticproject.services.TaskManagementServiceImpl;
+import java.util.HashMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -11,7 +16,18 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class SimpleElasticSuggestionBoxApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SimpleElasticSuggestionBoxApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(SimpleElasticSuggestionBoxApplication.class, args);
+
+        initializeTasks(applicationContext);
+
+    }
+
+    private static void initializeTasks(ApplicationContext applicationContext) {
+        TaskManagementService service = applicationContext.getBean(TaskManagementServiceImpl.class);
+        JobServerConfig config = applicationContext.getBean(JobServerConfig.class);
+        service.schedulePollFortasks();
+        service.scheduleTask(IndexJobType.UPDATE_INDEX, 300000, new HashMap());
+
     }
 
 }
